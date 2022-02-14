81dfc270d86835764682abd97a2bad76aa4082f2
Anu096279 <Anuwat57766@gmail.com>
2022-02-14 23:46:38 +0700

    add page about

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+function About() {
+   return (
+      <div className="about">
+         <div className="about-container">
+            <p>
+               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+               Praesentium cumque consequuntur in debitis sequi. Molestiae, et
+               minima! Animi corrupti recusandae voluptatem magnam blanditiis
+               rem, esse doloremque suscipit facilis eligendi? Sed?
+            </p>
+         </div>
+      </div>
+   );
+}
+
+export default About;
